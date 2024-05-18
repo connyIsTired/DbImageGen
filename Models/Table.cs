9c41cf1c6f060a1ce015a7f9fbe55da91eb6ffdd
Conny <conradkersten@gmail.com>
2024-05-17 22:17:23 -0500

    Update Table model to include list of fields

--- Models/Table.cs
+++ Models/Table.cs
@@ -2,5 +2,6 @@
 
 public class Table
 {
-	public string tableName {get; set;} = string.Empty;
+	public string tableName {get; init;} = string.Empty;
+	public List<string> fields {get; init;} = new List<string>();
 }
