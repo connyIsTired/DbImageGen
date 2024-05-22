79a805f8f3de2311022e290408da33763b345943
Conny <conradkersten@gmail.com>
2024-05-21 22:03:20 -0500

    Add ability to send in a title

--- Builder/Builder.cs
+++ Builder/Builder.cs
@@ -1,5 +1,4 @@
-﻿using DbImageGen.Models;
-namespace DbImageGen;
+﻿namespace DbImageGen;
 
 public class Builder
 {
@@ -12,12 +11,17 @@ public class Builder
 
 	public void BuildParentSvg()
 	{
-		returnObj = returnObj + "<svg width=\"1000\" height=\"1000\" xmlns=\"http://www.w3.org/2000/svg\">";
+		returnObj += "<svg width=\"1000\" height=\"1000\" xmlns=\"http://www.w3.org/2000/svg\">";
+	}
+
+	public void BuildOpenSvgTag()
+	{
+		returnObj += "<svg x=\"50\">";
 	}
 
 	public void BuildCloseSvgTag()
 	{
-		returnObj = returnObj + "</svg>";
+		 returnObj += "</svg>";
 	}
 
 	public string BuildText(FieldDto input)
@@ -26,21 +30,36 @@ public class Builder
 	    return svgText;
 	}
 
+	public void BuildLine()
+	{
+		returnObj += "<line x1=\"0\" x2=\"250\" y1=\"30\" y2=\"30\" stroke=\"black\"/>";
+	}
+
+	public void BuildTitle()
+	{
+
+		BuildOpenSvgTag();
+		returnObj += $"<text font-size=\"10pt\" x=\"125\" y=\"25\" class=\"small\" text-anchor=\"middle\">{_incoming.Tables.First().TableName}</text>";
+		BuildLine();
+		BuildCloseSvgTag();
+	}
+
 	public void BuildAllText()
 	{
-		returnObj = returnObj + _incoming.Tables.First().Fields.Aggregate(string.Empty, (result, next) => result = result + BuildText(next));
+		returnObj += _incoming.Tables.First().Fields.Aggregate(string.Empty, (result, next) => result = result + BuildText(next));
 
 	}
 
 	public void BuildRect()
 	{
-		returnObj = returnObj + "<rect x=\"50\" y=\"10\" width=\"250\" height=\"250\" fill=\"none\" stroke=\"blue\"/>";
+		returnObj += "<rect x=\"50\" y=\"10\" width=\"250\" height=\"250\" fill=\"none\" stroke=\"blue\"/>";
 	}
 	
 	public string Build()
 	{
 		BuildParentSvg();
 		BuildRect();
+		BuildTitle();
 		BuildAllText();
 		BuildCloseSvgTag();
 		return returnObj;
