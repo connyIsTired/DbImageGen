617140ba5c6e604d3eac38903752d149a4d8cf99
Conny <conradkersten@gmail.com>
2024-05-25 00:06:07 -0500

    Add table position property to TableDto

--- Models/TableDto.cs
+++ Models/TableDto.cs
@@ -5,4 +5,5 @@ public class TableDto
 	public string TableName { get; set; } = string.Empty;
 	public List<FieldDto> Fields { get; set; } = new List<FieldDto>();
 	public int TableSize { get; set; } = 0;
+	public TablePositions TablePositions { get; set; } = new TablePositions();
 }
