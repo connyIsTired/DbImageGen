0e6a42a62253c90f7f7db2468851fcb99fa529b7
Conny <conradkersten@gmail.com>
2024-07-11 22:59:16 -0500

    Add id and foreignKeys to request model

--- Models/Table.cs
+++ Models/Table.cs
@@ -4,4 +4,6 @@ public class Table
 {
 	public string tableName {get; init;} = string.Empty;
 	public List<string> fields {get; init;} = new List<string>();
+	public int id {get; init;} 
+	public List<int> foreignKeys {get; init;} = new List<int>();
 }
